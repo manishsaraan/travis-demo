fadf325e6698c47720779492f937493e3a83aeac
manishsaraan <manish021js@gmail.com>
2017-10-24 22:04:28 +0530

    linting changed from jshit to xo

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const handleServer = function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello Travis!\n');
+	res.end('Hello Travis!. This code is lint with XO.\n');
 };
 
 http.createServer(handleServer).listen(1337, '127.0.0.1');
